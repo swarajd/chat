a691e8dc341e23034db94906a730183c51dc21f8
swarage <sdcoolio@gmail.com>
2015-07-04 11:33:54 -0400

    added functionality to log messages

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '\\index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
