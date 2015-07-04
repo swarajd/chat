56ba071601cea9235dd09a73a301f5c900d42425
swarage <sdcoolio@gmail.com>
2015-07-04 14:24:20 -0400

    added nickname functionality

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
+//include all the dependancies
 var express = require('express');
 var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/swaraj-chat');
+var db = mongoose.connection;
+
+var userSchema = mongoose.Schema({
+  nick: String,
+  id: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+// include public assets
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
+//serve the main page
 app.get('/', function(req, res){
   res.sendFile(__dirname + '\\index.html');
 });
 
+//handle all the connections (might move this to a separate file since it's going to get big)
 io.on('connection', function(socket){
+
+  //just a regular chat message
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    var formatted_msg;
+    User.find({id: socket.id}, function(err, users) {
+      if (users.length === 0) {
+        formatted_msg = socket.id + ': ' + msg;
+        io.emit('chat message', formatted_msg);
+        console.log('message: ' + formatted_msg);
+      } else {
+        formatted_msg = users[0].nick + ': ' + msg;
+        io.emit('chat message', formatted_msg);
+        console.log('message: ' + formatted_msg);
+      }
+    });
+    
   });
+
+  //the user is trying to set his nick
+  socket.on('set nick', function(nick) {
+    User.find({nick: nick}, function(err, users){
+      if (users.length === 0) {
+        var tempuser = new User({
+          id: socket.id,
+          nick: nick
+        });
+        tempuser.save();
+        console.log('this nick is now taken by you!');
+      } else {
+        console.log('this nick exists: ' + nick);
+      }
+    });
+    console.log('tryna set a nick: ' + nick);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+    User.find({id: socket.id}).remove(function() {
+      console.log('going to free up this nick');
+    });
+  });
+
 });
 
+
+//serve the project
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,20 @@
+//create a socket
 var socket = io();
 
+//if the form is submitted, emit an event to be registered serverside
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
+  //parse input, will probably implement webpack and move the parsing to another file as well
+  var raw_input = $('#m').val();
+  if (raw_input.substring(0,5) === '/nick') {
+    socket.emit('set nick', raw_input.substring(6));
+  } else {
+    socket.emit('chat message', raw_input);
+  }
   $('#m').val('');
-  return false;
+  return false; 
 });
 
+//when the server emits the same event, append the message to the chat window
 socket.on('chat message', function(msg){
   $('#messages').append($('<li>').text(msg));
 });
