b7131c196f21f8fa85e69afb59bf5279ef0a430a
swarage <sdcoolio@gmail.com>
2015-07-04 10:44:41 -0400

    added functionality to see if user connects/dcs

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+var express = require('express');
+var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '\\index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
