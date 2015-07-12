8274eef0b93d2e317582ab8f7b0b795c09e53445
swarage <sdcoolio@gmail.com>
2015-07-12 18:59:13 -0400

    slowly merging both branches
    the current two branch system is really messy

--- index.js
+++ index.js
@@ -130,6 +130,6 @@ io.on('connection', function(socket){
 
 
 //serve the project
-http.listen(process.env.PORT || 5000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on whatever port');
 });
